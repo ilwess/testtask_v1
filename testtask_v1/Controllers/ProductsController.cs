ad9f57856cec3a9f7b55df060143df695b488356
ilwess <pasha.vrublevskiy20@list.ru>
2019-07-31 13:14:01 +0300

    add list of products

--- testtask_v1/Controllers/ProductsController.cs
+++ testtask_v1/Controllers/ProductsController.cs
@@ -21,6 +21,26 @@ namespace testtask_v1.Controllers
 
         public ViewResult List()
         {
+            List<Models.Product> products = new List<Models.Product>()
+            {
+                new Models.Product(0, "Pen", 1, "good pen)"),
+                new Models.Product(1, "Ball", 5, "the best ball ))"),
+                new Models.Product(2, "Plastic left leg", 10, "pretty good plastic leg for people without left leg"),
+                new Models.Product(3, "Plastic right leg", 10, "pretty good plastic right leg for people without right leg"),
+                new Models.Product(4, "Right and left plastic legs", 19, "nice set of legs with discount"),
+            };
+
+            string table = "Name        Price           Description" + Environment.NewLine;
+
+            foreach(Models.Product prod in products)
+            {
+                table += prod.Name + "      ";
+                table += prod.Price + "      ";
+                table += prod.Description + Environment.NewLine;
+            }
+
+            ViewBag.prodTable = table;
+            
             return View();
         }
     }
