ae9504c0ea1d9dcb29d573c6f7bcf5efd382fa10
ilwess <pasha.vrublevskiy20@list.ru>
2019-07-31 12:59:58 +0300

    add product model

---
+++ testtask_v1/Models/Product.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace testtask_v1.Models
+{
+    public class Product
+    {
+        private int id;
+        private string name;
+        private double price;
+        private string description;
+
+        public int Id
+        {
+            get { return id; }
+            private set { id = value; }
+        }
+
+        public string Name
+        {
+            get { return name; }
+            private set { name = value; }
+        }
+
+        public double Price
+        {
+            get { return price; }
+            private set { price = value; }
+        }
+
+        public string Description
+        {
+            get { return description; }
+            private set { description = value; }
+        }
+
+        public Product(int id, string name, double price, string descr)
+        {
+            this.id = id;
+            this.name = name;
+            this.price = price;
+            this.description = descr;
+        }
+    }
+}
