ab779156b3fa5561b01aa69da0864f289f68820d
ilwess <pasha.vrublevskiy20@list.ru>
2019-07-31 12:39:18 +0300

    Add customers controller

---
+++ testtask_v1/Controllers/CustomersController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace testtask_v1.Controllers
+{
+    public class CustomersController : Controller
+    {
+        // GET: Customers
+        public string Index()
+        {
+            return "Customers.Index";
+        }
+
+        public string Add()
+        {
+            return "Customers.Add";
+        }
+    }
+}
