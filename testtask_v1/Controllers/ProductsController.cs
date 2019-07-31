d8fcfa09d379abe3781a549fca80f8c8ef6c1683
ilwess <pasha.vrublevskiy20@list.ru>
2019-07-31 16:25:06 +0300

    modify add view and products controller

--- testtask_v1/Controllers/ProductsController.cs
+++ testtask_v1/Controllers/ProductsController.cs
@@ -3,11 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Data.Entity;
+using testtask_v1.Models;
 
 namespace testtask_v1.Controllers
 {
     public class ProductsController : Controller
     {
+        ProductContext pc = new ProductContext();
         // GET: Products
         public string Index()
         {
@@ -21,14 +24,7 @@ namespace testtask_v1.Controllers
 
         public ViewResult List()
         {
-            List<Models.Product> products = new List<Models.Product>()
-            {
-                new Models.Product(0, "Pen", 1, "good pen)"),
-                new Models.Product(1, "Ball", 5, "the best ball ))"),
-                new Models.Product(2, "Plastic left leg", 10, "pretty good plastic leg for people without left leg"),
-                new Models.Product(3, "Plastic right leg", 10, "pretty good plastic right leg for people without right leg"),
-                new Models.Product(4, "Right and left plastic legs", 19, "nice set of legs with discount"),
-            };
+            IEnumerable<Product> products = 
 
             ViewBag.prods = products;
             
