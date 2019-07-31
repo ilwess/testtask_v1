0795b5090b1169f37c42b57d6e1564ef96062fcc
ilwess <pasha.vrublevskiy20@list.ru>
2019-07-31 12:32:53 +0300

    add products controller

---
+++ testtask_v1/Controllers/ProductsController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace testtask_v1.Controllers
+{
+    public class ProductsController : Controller
+    {
+        // GET: Products
+        public string Index()
+        {
+            return "Products.Index";
+        }
+    }
+}
