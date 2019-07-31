13f215b0a31ee4fc4c940d55c0e4fda4e74bf4c6
ilwess <pasha.vrublevskiy20@list.ru>
2019-07-31 15:47:16 +0300

    Add product context

---
+++ testtask_v1/Models/ProductContext.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace testtask_v1.Models
+{
+    public class ProductContext : DbContext
+    {
+        public DbSet<Product> Prods { get; set; }
+    }
+}
