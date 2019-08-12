8316778ea400433cd6efa8ce43defd2ab4433921
ilwess <pasha.vrublevskiy20@list.ru>
2019-08-12 11:31:08 +0300

    Add role manager and role

---
+++ testtask_v1/Models/AppRole.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace testtask_v1.Models
+{
+    public class AppRole : IdentityRole
+    {
+        public AppRole() { }
+    }
+}
