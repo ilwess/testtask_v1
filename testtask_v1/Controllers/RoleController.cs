441aee52faf23703d54827fd5c1ad826056cb3b2
ilwess <pasha.vrublevskiy20@list.ru>
2019-08-12 11:49:19 +0300

    add Index to RoleController

--- testtask_v1/Controllers/RoleController.cs
+++ testtask_v1/Controllers/RoleController.cs
@@ -3,15 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using testtask_v1.Models;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.Owin;
 
 namespace testtask_v1.Controllers
 {
     public class RoleController : Controller
     {
+        private AppRoleManager roleManager
+        {
+            get { return HttpContext
+                    .GetOwinContext()
+                    .GetUserManager<AppRoleManager>(); }
+        }
         // GET: Role
         public ActionResult Index()
         {
-            return View();
+            return View(roleManager.Roles);
         }
     }
 }
